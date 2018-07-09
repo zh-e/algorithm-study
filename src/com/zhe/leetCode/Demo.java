7efad5f25a8eb6aac2968e8a0126f9db735d119c
zhangzhe <zhangzhe2@hualala.com>
2018-07-09 15:27:57 +0800

    init

---
+++ src/com/zhe/leetCode/Demo.java
@@ -0,0 +1,10 @@
+package com.zhe.leetCode;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Integer a = new Integer(10);
+        System.out.println(10 == a);
+    }
+
+}
