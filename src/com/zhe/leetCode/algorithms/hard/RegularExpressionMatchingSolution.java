b9c14613809b25e5f3ba0eeda4f0c36a6a52c76a
zhangzhe <zhangzhe@shuidihuzhu.com>
2019-04-29 15:24:37 +0800

    回文整数判断

---
+++ src/com/zhe/leetCode/algorithms/hard/RegularExpressionMatchingSolution.java
@@ -0,0 +1,21 @@
+package com.zhe.leetCode.algorithms.hard;
+
+import java.util.regex.Pattern;
+
+/**
+ * '.' 匹配任何单个字符。
+ * '*'匹配前面元素的零个或多个。
+ * @author zhangzhe
+ */
+public class RegularExpressionMatchingSolution {
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aaa", "a*"));
+    }
+
+    private static boolean isMatch(String s, String p) {
+
+        return false;
+    }
+
+}
