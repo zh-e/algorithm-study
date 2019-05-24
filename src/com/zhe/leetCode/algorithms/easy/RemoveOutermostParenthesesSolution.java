dfb02a6bae0fab7a2508a8b13dee68403f9b455c
zhangzhe <zz950702@163.com>
2019-05-24 11:20:32 +0800

    1021 去除最外括号

--- src/com/zhe/leetCode/algorithms/easy/RemoveOutermostParenthesesSolution.java
+++ src/com/zhe/leetCode/algorithms/easy/RemoveOutermostParenthesesSolution.java
@@ -1,9 +1,5 @@
 package com.zhe.leetCode.algorithms.easy;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
-
 /**
  * 1021 去除外部括号
  * @author zhangzhe
@@ -12,12 +8,25 @@ public class RemoveOutermostParenthesesSolution {
 
     public static void main(String[] args) {
         System.out.println(removeOuterParentheses("(()())(())(()(()))"));
+        //System.out.println(removeOuterParentheses("(()(()))"));
     }
 
-    public static String removeOuterParentheses(String S) {
-        // (()()) (()) (()(()))
+    private static String removeOuterParentheses(String S) {
+        StringBuilder res = new StringBuilder();
+        int left = 0;
+        for(char c : S.toCharArray()) {
+            if (c == '(') {
+                ++left;
+            }
+            if (left > 1) {
+                res.append(c);
+            }
+            if (c == ')') {
+                left--;
+            }
 
-        return "";
+        }
+        return res.toString();
     }
 
 }
