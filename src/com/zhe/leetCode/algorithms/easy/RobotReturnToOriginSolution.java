d4190480d7f85cffbf041b20d0951f70eac8f124
zhangzhe <zz950702@163.com>
2019-05-25 22:45:44 +0800

    日常打卡  两道easy

---
+++ src/com/zhe/leetCode/algorithms/easy/RobotReturnToOriginSolution.java
@@ -0,0 +1,40 @@
+package com.zhe.leetCode.algorithms.easy;
+
+/**
+ * 657. Robot Return to Origin
+ *
+ * @author zhangzhe
+ */
+public class RobotReturnToOriginSolution {
+
+    public static void main(String[] args) {
+        String moves = "LR";
+        System.out.println(judgeCircle(moves));
+    }
+
+    private static boolean judgeCircle(String moves) {
+
+        int i = 0;
+        int j = 0;
+        for (char c : moves.toCharArray()) {
+            switch (c) {
+                case 'L':
+                    i--;
+                    break;
+                case 'R':
+                    i++;
+                    break;
+                case 'U':
+                    j++;
+                    break;
+                case 'D':
+                    j--;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return i == 0 && j == 0;
+    }
+
+}
