f52cc1fcdefab604f8630e49f0e06afc1adb4733
zhangzhe <zz950702@163.com>
2019-05-30 11:09:00 +0800

    easy 509

---
+++ src/com/zhe/leetCode/algorithms/easy/FibonacciNumberSolution.java
@@ -0,0 +1,22 @@
+package com.zhe.leetCode.algorithms.easy;
+
+/**
+ * 509. Fibonacci Number
+ *
+ * @author zhangzhe
+ */
+public class FibonacciNumberSolution {
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(fib(n));
+    }
+
+    private static int fib(int N) {
+        if(N == 0 || N== 1) {
+            return N;
+        }
+        return fib(N - 1) + fib(N - 2);
+    }
+
+}
