b3c691c0c1174a5a262be2a877b8868bc818b2d9
zhangzhe <zz950702@163.com>
2019-06-04 15:22:11 +0800

    811

---
+++ src/com/zhe/leetCode/algorithms/easy/SubdomainVisitCountSolution.java
@@ -0,0 +1,51 @@
+package com.zhe.leetCode.algorithms.easy;
+
+import com.zhe.leetCode.algorithms.base.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 811. Subdomain Visit Count
+ *
+ * @author zhangzhe
+ */
+public class SubdomainVisitCountSolution {
+
+    public static void main(String[] args) {
+        String[] cpdomains = {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
+        CollectionUtils.printList(subdomainVisits(cpdomains));
+    }
+
+    private static List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> resMap = new HashMap<>();
+        for (String str : cpdomains) {
+            String[] s = str.split(" ");
+            int time = Integer.valueOf(s[0]);
+            str = s[1];
+            while (str != null) {
+                Integer n = resMap.get(str);
+                if(n == null) {
+                    resMap.put(str, time);
+                } else {
+                    resMap.put(str, n + time);
+                }
+                int i = str.indexOf(".");
+                if(i > 0) {
+                    str = str.substring(str.indexOf(".") + 1);
+                } else {
+                    str = null;
+                }
+            }
+        }
+
+        List<String> resList = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry : resMap.entrySet()) {
+            resList.add(entry.getValue() + " " + entry.getKey());
+        }
+        return resList;
+    }
+
+}
