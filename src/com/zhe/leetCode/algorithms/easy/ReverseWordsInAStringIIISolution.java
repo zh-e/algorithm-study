73db9c6d1e22aa9a513b2a87db7e774233ae26fe
zhangzhe <zz950702@163.com>
2019-06-05 10:48:35 +0800

    557

---
+++ src/com/zhe/leetCode/algorithms/easy/ReverseWordsInAStringIIISolution.java
@@ -0,0 +1,24 @@
+package com.zhe.leetCode.algorithms.easy;
+
+/**
+ * 557. Reverse Words in a String III
+ *
+ * @author zhangzhe
+ */
+public class ReverseWordsInAStringIIISolution {
+
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        System.out.println(reverseWords(s));
+    }
+
+    private static String reverseWords(String s) {
+        String[] strs = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(String str : strs) {
+            sb.append(new StringBuilder(str).reverse()).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+}
