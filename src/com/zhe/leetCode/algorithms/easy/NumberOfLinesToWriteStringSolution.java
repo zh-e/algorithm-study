015b2a9bd2be10d8e8e525ac4d08cbfcba0e00d5
zhangzhe <zz950702@163.com>
2019-06-09 20:27:04 +0800

    easy的几道题

---
+++ src/com/zhe/leetCode/algorithms/easy/NumberOfLinesToWriteStringSolution.java
@@ -0,0 +1,32 @@
+package com.zhe.leetCode.algorithms.easy;
+
+import com.zhe.leetCode.algorithms.base.ArrayUtils;
+
+/**
+ * 806. Number of Lines To Write String
+ *
+ * @author zhangzhe
+ */
+public class NumberOfLinesToWriteStringSolution {
+
+    public static void main(String[] args) {
+        int[] widths = {4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        String S = "bbbcccdddaaa";
+        ArrayUtils.printArry(numberOfLines(widths, S));
+    }
+
+    private static int[] numberOfLines(int[] widths, String S) {
+        int line = 1, lineLen = 0;
+        for (char c : S.toCharArray()) {
+            int charLen = widths[c - 'a'];
+            if (lineLen + charLen > 100) {
+                line++;
+                lineLen = charLen;
+            } else {
+                lineLen += charLen;
+            }
+        }
+        return new int[]{line, lineLen};
+    }
+
+}
---
+++ src/com/zhe/leetCode/algorithms/easy/RemoveAllAdjacentDuplicatesInStringSolution.java
@@ -0,0 +1,34 @@
+package com.zhe.leetCode.algorithms.easy;
+
+import java.util.Stack;
+
+/**
+ * 1047. Remove All Adjacent Duplicates In String
+ *
+ * @author zhangzhe
+ */
+public class RemoveAllAdjacentDuplicatesInStringSolution {
+
+    public static void main(String[] args) {
+        String S = "abbaca";
+        System.out.println(removeDuplicates(S));
+    }
+
+    private static String removeDuplicates(String S) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : S.toCharArray()) {
+            if(!stack.empty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+}
