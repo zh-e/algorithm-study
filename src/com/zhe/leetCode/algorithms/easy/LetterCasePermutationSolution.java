212bb2bea61d554166bd18ff7f2e1c4573ce2dd9
zhangzhe <zz950702@163.com>
2019-06-15 23:09:47 +0800

    init 784

---
+++ src/com/zhe/leetCode/algorithms/easy/LetterCasePermutationSolution.java
@@ -0,0 +1,29 @@
+package com.zhe.leetCode.algorithms.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 784. Letter Case Permutation
+ *
+ * @author zhangzhe
+ */
+public class LetterCasePermutationSolution {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static List<String> letterCasePermutation(String S) {
+        char[] strChar = S.toCharArray();
+        List<String> ans = new ArrayList<>();
+        for (int i = 0, len = strChar.length; i < len; i++) {
+            if(strChar[i] >= 'a' && strChar[i] <= 'Z') {
+
+            }
+        }
+
+        return null;
+    }
+
+}
