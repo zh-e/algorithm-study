49f988016117da29a0e1e87a9d35d43c40071d00
zhangzhe <zz950702@163.com>
2019-06-16 19:43:50 +0800

    easy 1089

---
+++ src/com/zhe/leetCode/algorithms/easy/DuplicateZerosSolution.java
@@ -0,0 +1,30 @@
+package com.zhe.leetCode.algorithms.easy;
+
+import com.zhe.leetCode.algorithms.base.ArrayUtils;
+
+/**
+ * 1089. Duplicate Zeros
+ * @author zhangzhe
+ */
+public class DuplicateZerosSolution {
+
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,0,0,0,0};
+        duplicateZeros(arr);
+        ArrayUtils.printArry(arr);
+    }
+
+    private static void duplicateZeros(int[] arr) {
+        int[] ans  = new int[arr.length];
+        for(int i = 0, j = 0, len = arr.length; i < len && j < len; j++, i++) {
+            ans[j] = arr[i];
+            if(arr[i] == 0 && ++j < len) {
+                ans[j] = 0;
+            }
+        }
+        for(int i = 0, len = arr.length; i < len; i++) {
+            arr[i] = ans[i];
+        }
+    }
+
+}
