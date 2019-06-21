65e47d63f5479d81c0994600b56a694142c050bb
zhangzhe <zz950702@163.com>
2019-06-21 20:57:25 +0800

    easy:784,937

--- src/com/zhe/leetCode/algorithms/easy/ReorderLogFilesSolution.java
+++ src/com/zhe/leetCode/algorithms/easy/ReorderLogFilesSolution.java
@@ -1,12 +1,37 @@
 package com.zhe.leetCode.algorithms.easy;
 
+import com.zhe.leetCode.algorithms.base.ArrayUtils;
+
+import java.util.Arrays;
+
 /**
+ * 937. Reorder Log Files
+ *
  * @author zhangzhe
  */
 public class ReorderLogFilesSolution {
 
+    public static void main(String[] args) {
+        String[] logs = {"a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"};
+        ArrayUtils.printArry(reorderLogFiles(logs));
+    }
+
     private static String[] reorderLogFiles(String[] logs) {
-        return null;
+        Arrays.sort(logs, (log1, log2) -> {
+            String[] split1 = log1.split(" ", 2);
+            String[] split2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+            if (!isDigit1 && !isDigit2) {
+                int cmp = split1[1].compareTo(split2[1]);
+                if (cmp != 0) {
+                    return cmp;
+                }
+                return split1[0].compareTo(split2[0]);
+            }
+            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+        });
+        return logs;
     }
 
 }
