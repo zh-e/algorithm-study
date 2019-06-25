1c31237497701dd164c0403cc4ae0c9e409883df
zhangzhe <zz950702@163.com>
2019-06-25 14:18:52 +0800

    easy:521

---
+++ src/com/zhe/leetCode/algorithms/easy/DesignHashMapSolution.java
@@ -0,0 +1,41 @@
+package com.zhe.leetCode.algorithms.easy;
+
+/**
+ * 706. Design HashMap
+ *
+ * @author zhangzhe
+ */
+public class DesignHashMapSolution {
+
+    class MyHashMap {
+
+        /** Initialize your data structure here. */
+        public MyHashMap() {
+
+        }
+
+        /** value will always be non-negative. */
+        public void put(int key, int value) {
+
+        }
+
+        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+        public int get(int key) {
+
+        }
+
+        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+        public void remove(int key) {
+
+        }
+    }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+
+}
---
+++ src/com/zhe/leetCode/algorithms/easy/LongestUncommonSubsequenceISolution.java
@@ -0,0 +1,25 @@
+package com.zhe.leetCode.algorithms.easy;
+
+/**
+ * 521. Longest Uncommon Subsequence I
+ *
+ * @author zhangzhe
+ */
+public class LongestUncommonSubsequenceISolution {
+
+    public static void main(String[] args) {
+        String a = "aba";
+        String b = "cdc";
+        System.out.println(findLUSlength(a, b));
+    }
+
+    private static int findLUSlength(String a, String b) {
+
+        if(a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
+
+    }
+
+}
