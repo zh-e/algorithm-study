ee51012a3791067dee339970596baac2f7af1a2c
zhangzhe <zz950702@163.com>
2019-06-29 17:58:53 +0800

    easy:292

---
+++ src/com/zhe/leetCode/algorithms/easy/NimGameSolution.java
@@ -0,0 +1,18 @@
+package com.zhe.leetCode.algorithms.easy;
+
+/**
+ * 292. Nim Game
+ *
+ * @author zhangzhe
+ */
+public class NimGameSolution {
+
+    public static void main(String[] args) {
+        System.out.println(canWinNim(4));
+    }
+
+    private static boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+
+}
