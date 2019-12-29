ca301a658bd5387293d23d4a667470353c0b9419
zhangzhe <zz950702@163.com>
2019-12-29 12:10:46 +0800

    easy

--- src/com/zhe/leetCode/algorithms/base/ListNode.java
+++ src/com/zhe/leetCode/algorithms/base/ListNode.java
@@ -12,4 +12,23 @@ public class ListNode {
         val = x;
     }
 
+    public static ListNode init(int... num) {
+        ListNode head = new ListNode(0);
+        ListNode node = head;
+        for (int n : num) {
+            node.next = new ListNode(n);
+            node = node.next;
+        }
+        return head.next;
+    }
+
+    public static void toString(ListNode head) {
+        System.out.print(head.val);
+        while (head.next != null) {
+            System.out.print(" --> ");
+            System.out.print(head.next.val);
+            head = head.next;
+        }
+    }
+
 }
