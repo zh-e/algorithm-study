e3f6a7a8ffaa73cf3cc25bb0c4c5998fc714e542
zhangzhe <zz950702@163.com>
2020-01-03 17:05:47 +0800

    155 最小数栈

---
+++ src/com/zhe/leetCode/algorithms/easy/MinStackSolution.java
@@ -0,0 +1,59 @@
+package com.zhe.leetCode.algorithms.easy;
+
+import java.util.Stack;
+
+/**
+ * 155. Min Stack
+ *
+ * @author zhangzhe
+ */
+public class MinStackSolution {
+
+	public static void main(String[] args) {
+		MinStack minStack = new MinStack();
+		minStack.push(-2);
+		minStack.push(0);
+		minStack.push(-3);
+		System.out.println(minStack.getMin());
+		minStack.pop();
+		minStack.top();
+		System.out.println(minStack.getMin());
+	}
+
+}
+
+class MinStack {
+
+	private Stack<Integer> cache;
+	private Stack<Integer> min;
+
+	public MinStack() {
+		cache = new Stack<>();
+		min = new Stack<>();
+	}
+
+	public void push(int x) {
+		cache.push(x);
+		if (min.isEmpty() || min.peek() >= x) {
+			min.push(x);
+		}
+	}
+
+	public void pop() {
+		if (cache.isEmpty()) {
+			return;
+		}
+		if (min.peek().equals(cache.pop())) {
+			min.pop();
+		}
+	}
+
+	public int top() {
+		return cache.peek();
+	}
+
+	public int getMin() {
+		return min.peek();
+	}
+
+}
