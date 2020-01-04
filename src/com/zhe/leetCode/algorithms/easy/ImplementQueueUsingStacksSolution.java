96cf116d281f329919ba584c1183af21b30b8c7c
zhangzhe <zz950702@163.com>
2020-01-04 13:00:22 +0800

    .

---
+++ src/com/zhe/leetCode/algorithms/easy/ImplementQueueUsingStacksSolution.java
@@ -0,0 +1,40 @@
+package com.zhe.leetCode.algorithms.easy;
+
+/**
+ * 232. Implement Queue using Stacks
+ */
+public class ImplementQueueUsingStacksSolution {
+
+	public static void main(String[] args) {
+
+	}
+
+	class MyQueue {
+
+		/** Initialize your data structure here. */
+		public MyQueue() {
+
+		}
+
+		/** Push element x to the back of queue. */
+		public void push(int x) {
+
+		}
+
+		/** Removes the element from in front of queue and returns that element. */
+		public int pop() {
+
+		}
+
+		/** Get the front element. */
+		public int peek() {
+
+		}
+
+		/** Returns whether the queue is empty. */
+		public boolean empty() {
+
+		}
+	}
+
+}
