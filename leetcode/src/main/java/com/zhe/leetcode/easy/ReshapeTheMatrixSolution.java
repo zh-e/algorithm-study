44d0f75af9a7b149ea9b6d069f303aee2f68440f
张哲 <zz950702@163.com>
2022-01-13 00:19:51 +0800

    leetcode-566

---
+++ leetcode/src/main/java/com/zhe/leetcode/easy/ReshapeTheMatrixSolution.java
@@ -0,0 +1,44 @@
+package com.zhe.leetcode.easy;
+
+
+import com.zhe.leetcode.base.ArrayUtils;
+
+/**
+ * 566. Reshape the Matrix
+ *
+ * @author zhangzhe
+ */
+public class ReshapeTheMatrixSolution {
+
+    public static void main(String[] args) {
+        int[][] nums = {
+                {1, 2},
+                {3, 4}
+        };
+        int r = 1, c = 4;
+        ArrayUtils.printArry(matrixReshape(nums, r, c));
+    }
+
+    private static int[][] matrixReshape(int[][] nums, int r, int c) {
+        int rLen = nums.length;
+        int cLen = nums[0].length;
+        if (r >= rLen && c >= cLen) {
+            return nums;
+        }
+
+        int[][] ans = new int[r][c];
+        int nr = 0, nc = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (nc >= cLen) {
+                    nr++;
+                    nc = 0;
+                }
+                ans[i][j] = nums[nr][nc];
+                nc++;
+            }
+        }
+        return ans;
+    }
+
+}
