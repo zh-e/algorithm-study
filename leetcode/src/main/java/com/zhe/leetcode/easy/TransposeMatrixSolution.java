47fdd3d3ef23746f82d49acc35a8a6570501d2f7
张哲 <zz950702@163.com>
2022-01-28 00:23:54 +0800

    leetcode-867

---
+++ leetcode/src/main/java/com/zhe/leetcode/easy/TransposeMatrixSolution.java
@@ -0,0 +1,29 @@
+package com.zhe.leetcode.easy;
+
+import com.zhe.leetcode.base.ArrayUtils;
+
+/**
+ * 867. Transpose Matrix
+ *
+ * @author zhangzhe
+ */
+public class TransposeMatrixSolution {
+
+    public static void main(String[] args) {
+        int[][] A = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        ArrayUtils.printArry(transpose(A));
+    }
+
+    private static int[][] transpose(int[][] A) {
+        int rLen = A.length;
+        int cLen = A[0].length;
+        int[][] res = new int[cLen][rLen];
+        for (int i = 0; i < rLen; i++) {
+            for (int j = 0; j < cLen; j++) {
+                res[j][i] = A[i][j];
+            }
+        }
+        return res;
+    }
+
+}
