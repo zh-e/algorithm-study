34f9f16a3f06d05f9ca9d62650e9646e01773791
张哲 <zz950702@163.com>
2022-02-04 00:23:54 +0800

    .

---
+++ leetcode/src/main/java/com/zhe/leetcode/hard/RegularExpressionMatchingSolution.java
@@ -0,0 +1,19 @@
+package com.zhe.leetcode.hard;
+
+/**
+ * '.' 匹配任何单个字符。
+ * '*'匹配前面元素的零个或多个。
+ * @author zhangzhe
+ */
+public class RegularExpressionMatchingSolution {
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aaa", "a*"));
+    }
+
+    private static boolean isMatch(String s, String p) {
+
+        return false;
+    }
+
+}
