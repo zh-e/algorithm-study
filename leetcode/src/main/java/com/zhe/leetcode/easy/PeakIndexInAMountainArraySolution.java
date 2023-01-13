0433a7af2f10a1223f84a315a13f27551cffa7ea
张哲 <zz950702@163.com>
2023-01-14 00:23:54 +0800

    leetcode-852

---
+++ leetcode/src/main/java/com/zhe/leetcode/easy/PeakIndexInAMountainArraySolution.java
@@ -0,0 +1,26 @@
+package com.zhe.leetcode.easy;
+
+/**
+ * 852. Peak Index in a Mountain Array
+ *
+ * @author zhangzhe
+ */
+public class PeakIndexInAMountainArraySolution {
+
+    public static void main(String[] args) {
+        int[] A = {3, 4, 5, 1};
+        System.out.println(peakIndexInMountainArray(A));
+    }
+
+    private static int peakIndexInMountainArray(int[] A) {
+        int tmp = 0;
+        int res = 0;
+        for (int i = 0, len = A.length; i < len; i++) {
+            if (tmp < A[i]) {
+                tmp = A[i];
+                res = i;
+            }
+        }
+        return res;
+    }
+}
