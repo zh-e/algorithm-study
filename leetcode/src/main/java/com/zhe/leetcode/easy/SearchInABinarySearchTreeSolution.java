610cdc1cf8b6e3592ba7fa8d0d9f92a10fbfcfda
张哲 <zz950702@163.com>
2023-02-26 00:23:54 +0800

    leetcode-700

---
+++ leetcode/src/main/java/com/zhe/leetcode/easy/SearchInABinarySearchTreeSolution.java
@@ -0,0 +1,51 @@
+package com.zhe.leetcode.easy;
+
+import com.zhe.leetcode.base.TreeNode;
+
+/**
+ * 700. Search in a Binary Search Tree
+ *
+ * @author zhangzhe
+ */
+public class SearchInABinarySearchTreeSolution {
+
+    public static void main(String[] args) {
+        TreeNode root = initNode();
+        TreeNode res = searchBST(root, 2);
+        TreeNode.inOrderTraversal(res);
+    }
+
+    private static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+
+    private static TreeNode initNode() {
+        TreeNode node = new TreeNode(4);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(7);
+        TreeNode node3 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(3);
+
+        node.left = node1;
+        node.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+
+        return node;
+    }
+
+
+}
+
