fae3055fc9cacb76cc0c96b96f72a5bb4e165190
zh-e <zz950702@163.com>
2023-03-17 00:23:55 +0800

    402 移掉K位数字

---
+++ leetcode/src/main/java/com/zhe/leetcode/medium/RemoveKDigitsSolution.java
@@ -0,0 +1,49 @@
+package com.zhe.leetcode.medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 402. 移掉 K 位数字
+ * 给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
+ */
+public class RemoveKDigitsSolution {
+
+    public static void main(String[] args) {
+        String num = "1432219";
+        int k = 3;
+        System.out.println(new RemoveKDigitsSolution().removeKdigits(num, k));
+    }
+
+    public String removeKdigits(String num, int k) {
+        Deque<Character> deque = new LinkedList<>();
+        int length = num.length();
+        for (int i = 0; i < length; i++) {
+            char digit = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
+                deque.pollLast();
+                k--;
+            }
+            deque.offerLast(digit);
+        }
+
+        for (int i = 0; i < k; i++) {
+            deque.pollLast();
+        }
+
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        while (!deque.isEmpty()) {
+            char digit = deque.pollFirst();
+            if (leadingZero && digit == '0') {
+                continue;
+            }
+            leadingZero = false;
+            ret.append(digit);
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
+
+
+    }
+
+}
