0939a03354f703635d7693450e0673de63492b84
zhangzhe <zhangzhe_1@moqipobing>
2023-03-23 00:23:55 +0800

    103 198 哈哈

--- leetcode/src/main/java/com/zhe/leetcode/base/TreeNode.java
+++ leetcode/src/main/java/com/zhe/leetcode/base/TreeNode.java
@@ -12,8 +12,18 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() {
+
+    }
+
     public TreeNode(int x) {
         val = x;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 
     /**
