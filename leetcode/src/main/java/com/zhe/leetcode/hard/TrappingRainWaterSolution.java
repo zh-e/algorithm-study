30214a731bd7e1a30bc4a3ed6f35e7105fc001ba
zhangzhe <zhangzhe_1@moqipobing>
2023-03-28 00:23:55 +0800

    42 接雨水

---
+++ leetcode/src/main/java/com/zhe/leetcode/hard/TrappingRainWaterSolution.java
@@ -0,0 +1,67 @@
+package com.zhe.leetcode.hard;
+
+import java.util.Arrays;
+
+/**
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ */
+public class TrappingRainWaterSolution {
+
+    public static void main(String[] args) {
+//        int[] height = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+
+        int[] height = new int[]{4, 2, 0, 3, 2, 5};
+        System.out.println(new TrappingRainWaterSolution().trap(height));
+
+    }
+
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        System.out.println(Arrays.toString(leftMax));
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        System.out.println(Arrays.toString(rightMax));
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+    public int trap1(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+
+
+}
