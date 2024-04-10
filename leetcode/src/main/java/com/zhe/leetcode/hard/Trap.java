a184ce799423a3934a31551f1d0347ced7688f22
zh-e <zz950702@163.com>
2024-04-10 16:52:40 +0800

    接雨水

---
+++ leetcode/src/main/java/com/zhe/leetcode/hard/Trap.java
@@ -0,0 +1,89 @@
+package com.zhe.leetcode.hard;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ * 示例 2：
+ * <p>
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ */
+public class Trap {
+
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i++) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.max(leftMax[i], rightMax[i] - height[i]);
+        }
+
+        return ans;
+    }
+
+    public int trap1(int[] height) {
+        int ans = 0;
+
+        Deque<Integer> stack = new LinkedList<>();
+        int n = height.length;
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int left = stack.peek();
+                int currWidth = i - left - 1;
+                int currHeight = Math.min(height[left], height[i] - height[top]);
+                ans += currWidth * currHeight;
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+
+    public int trap2(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                left++;
+            } else {
+                ans += rightMax - height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+
+}
