3ce66b89743266543b0e582dfc086ce343945545
zh-e <zz950702@163.com>
2024-04-16 22:01:43 +0800

    ..

---
+++ leetcode/src/main/java/com/zhe/leetcode/medium/MergeSolution.java
@@ -0,0 +1,61 @@
+package com.zhe.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 56. 合并区间
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+ * 请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2：
+ * <p>
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 104
+ */
+public class MergeSolution {
+
+    public static void main(String[] args) {
+        int[][] intervals = new int[][]{{1, 3}, {2, 6}, {8, 10}};
+
+    }
+
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+
+        List<int[]> merged = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            int l = interval[0], r = interval[1];
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < l) {
+                merged.add(new int[]{l, r});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], r);
+            }
+        }
+
+        return merged.toArray(new int[][]{});
+    }
+
+}
